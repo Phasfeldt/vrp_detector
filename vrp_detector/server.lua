9571dbd6eb73698e9163615a5e8de506121a9827
Phasfeldt <hasfeldt@yahoo.dk>
2018-07-26 11:52:31 +0200

    Small fix.

--- vrp_detector/server.lua
+++ vrp_detector/server.lua
@@ -14,7 +14,9 @@ AddEventHandler('checkthis', function()
     local cop = vRP.hasGroup({user_id,"police"}) --Checks for police-group //Change this, to the name of the whitelist-group if not 'police'
     local ems = vRP.hasGroup({user_id,"ems"}) --Checks for ems-group //Change this, to the name of the whitelist-group if not 'ems'
     
-	if not cop or not ems then --Checks to see, if you're police or ems. If not, runthat triggers.
+	if cop or ems then --Checks to see, if you're police or ems. If not, runthat triggers.
+	--Nothing runs, because you're allowed to pass through. Could put another trigger here?
+	else
       TriggerClientEvent('runthat',player)
     end
 end)
